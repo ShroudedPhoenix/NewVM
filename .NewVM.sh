7b5a8ccf945222cf5df117120cfbe5a526933e6a
ShroudedPhoenix <65506812+ShroudedPhoenix@users.noreply.github.com>
2020-05-30 12:50:34 -0500

    Update .NewVM.sh
    Added python3-pip install

--- .NewVM.sh
+++ .NewVM.sh
@@ -34,6 +34,7 @@ function PythonInstall() {
     sudo apt-get install build-essential git
     sudo apt install python
     sudo apt install python3
+    sudo apt-get install python3-pip
     sudo apt-get install curl
     curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
     python get-pip.py
